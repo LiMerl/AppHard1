48d98f1bc6eb00ab6675b8380a91f6a039f3137b
Merlin LIMON <merlinlimon@gmail.com>
2022-12-02 15:39:36 +0100

    Spring initializer

---
+++ src/main/java/org/polytech/AppHard/AppHardApplication.java
@@ -0,0 +1,13 @@
+package org.polytech.AppHard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppHardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/polytech/AppHard/AppHardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.polytech.AppHard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppHardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
